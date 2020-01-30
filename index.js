56c68559599c5fc58dae05835a72fb8666126185
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2020-01-30 16:01:15 -0600

    added env variables

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const server = require('./server.js');
 
 const PORT = process.env.PORT || 3300;
+const HOST = process.env.HOST || "0.0.0.0"
+
+server.use((req, res, next) => {
+  console.log(`{}[${new Date().toLocaleString()}] ${req.id} ${req.method} ${req.url}`)
+  next()
+})
 
 server.use((err, req, res, next) => {
   console.log(err)
@@ -9,6 +15,6 @@ server.use((err, req, res, next) => {
   })
 })
 
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+server.listen(PORT, HOST, () => {
+  console.log(`\n=== Server listening on http://${HOST}:${PORT} ===\n`);
 });
