a01df2fb7e762900002cf80722f5033705e10d61
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2020-01-31 15:34:17 -0600

    test

--- data/seeds/000-clean-up.js
+++ data/seeds/000-clean-up.js
@@ -8,11 +8,11 @@
 //   });
 // };
 exports.seed = async function(knex, Promise) {
+	await knex('drivers').del()
+	await knex('customers').del()
 	await knex('reviews').del()
 	// await knex.raw('ALTER TABLE ' + 'reviews' + ' AUTO_INCREMENT = 1')
-	await knex('customers').del()
 	// await knex.raw('ALTER TABLE ' + 'customers' + ' AUTO_INCREMENT = 1')
-	await knex('drivers').del()
 	await knex.raw('ALTER TABLE ' + 'drivers' + ' AUTO_INCREMENT = 1')
 	
 }
