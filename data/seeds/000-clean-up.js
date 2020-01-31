4c5180f92f6367c5d7c2e0fabfa63dee90987b85
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2020-01-31 15:31:24 -0600

    fixed errors

--- data/seeds/000-clean-up.js
+++ data/seeds/000-clean-up.js
@@ -9,9 +9,9 @@
 // };
 exports.seed = async function(knex, Promise) {
 	await knex('reviews').del()
-	await knex.raw('ALTER TABLE ' + 'reviews' + ' AUTO_INCREMENT = 1')
+	// await knex.raw('ALTER TABLE ' + 'reviews' + ' AUTO_INCREMENT = 1')
 	await knex('customers').del()
-	await knex.raw('ALTER TABLE ' + 'customers' + ' AUTO_INCREMENT = 1')
+	// await knex.raw('ALTER TABLE ' + 'customers' + ' AUTO_INCREMENT = 1')
 	await knex('drivers').del()
 	await knex.raw('ALTER TABLE ' + 'drivers' + ' AUTO_INCREMENT = 1')
 	
