4630c52be7d9a6769c9f5dbe965b925efcd44644
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2020-01-31 15:12:42 -0600

    change seed files

--- data/seeds/000-clean-up.js
+++ data/seeds/000-clean-up.js
@@ -1,8 +1,18 @@
 
-const cleaner = require("knex-cleaner");
+// const cleaner = require("knex-cleaner");
 
-exports.seed = function(knex) {
-  return cleaner.clean(knex, {
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
-  });
-};
+
+// exports.seed = function(knex) {
+//   return cleaner.clean(knex, {
+//      ignoreTables: ["knex_migrations", "knex_migrations_lock", "spatial_ref_sys"]
+//   });
+// };
+exports.seed = async function(knex, Promise) {
+	await knex('reviews').del()
+	await knex.raw('ALTER TABLE ' + 'reviews' + ' AUTO_INCREMENT = 1')
+	await knex('customers').del()
+	await knex.raw('ALTER TABLE ' + 'customers' + ' AUTO_INCREMENT = 1')
+	await knex('drivers').del()
+	await knex.raw('ALTER TABLE ' + 'drivers' + ' AUTO_INCREMENT = 1')
+	
+}
--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   production: {
     client: 'pg',
     useNullAsDefault: true,
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
