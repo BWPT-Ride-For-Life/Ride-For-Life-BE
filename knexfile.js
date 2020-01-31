99bce1db27bc2bcf8a89887ab858cd45f387b965
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2020-01-31 17:39:31 -0600

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,14 @@ module.exports = {
   production: {
     client: 'pg',
     useNullAsDefault: true,
-    connection: process.env.DB_URL,
+    connection: {
+      DATABASE_URL: process.env.DB_URL,
+      DB_HOST: process.env.DB_HOST,
+      DB_NAME: process.env.DB_NAME,
+      DB_USER: process.env.DB_USER,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_PORT: process.env.DB_PORT,
+    },
     migrations: {
       directory: './data/migrations',
     },
